fa92b618e7335183a298066f3e1be054c5a05f91
Aoi <tsuno-a@aidemy.net>
2019-09-14 14:44:10 +0900

    update 色々やった

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import { slashcommand, slashcommand2 } from './apis/slack_cf';
-import { createMeetup } from './apis/cf_db';
+import { slashcommand, slashcommand2 } from "./apis/slack_cf";
+import { helloWorld, posting, updatePosting } from "./apis/cf_db";
 
-export { slashcommand, slashcommand2, createMeetup };
+export { slashcommand, slashcommand2, helloWorld, posting, updatePosting };
