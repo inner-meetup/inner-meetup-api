05e02f8b848ba9fc8c4cf0c6db2e7cc5abd0f84d
moriyama-k <moriyama-k@aidemy.net>
2019-09-14 17:30:07 +0900

    slackの連携等行った

--- functions/src/apis/slack_cf.ts
+++ functions/src/apis/slack_cf.ts
@@ -1,6 +1,10 @@
 import * as functions from "firebase-functions";
 const cors = require("cors")({ origin: true });
 import * as rp from "request-promise";
+import * as shortid from "shortid";
+import { WebClient } from "@slack/client";
+const accessToken =
+  "xoxp-748777362451-753838114625-762107818471-95fe22b54efcf0b55a2a3b48b5de8b75";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -28,17 +32,19 @@ export const addMeetup = functions.https.onRequest(async (req, res) => {
       return;
     }
 
+
+    const postingId = shortid.generate();
     // 実際に登録する
     try {
       await rp({
         method: "POST",
         uri: " https://us-central1-inner-meetup.cloudfunctions.net/posting",
         body: {
-          postingId: "aaaa", // 投稿のID（tsと同じでよき）
+          postingId: shortid.generate(), // 投稿のID（tsと同じでよき）
           description, // 投稿内容
           postingType, // give/take
           userId: user_id, // 投稿したひとのSlackID
-          channel_id: channel_id // 投稿されたチャンネルID
+          channel: channel_id // 投稿されたチャンネルID
         },
         json: true // Automatically stringifies the body to JSON
       });
@@ -52,13 +58,13 @@ export const addMeetup = functions.https.onRequest(async (req, res) => {
     }
 
     const resText = `
-*勉強会が申し込まれました！*
+*勉強会が申し込まれました！* 
 \`立案者:\` ${userSlackName}
 \`種別:\` ${postingType}
 \`内容:\` ${description}
 
 興味のある方はreactionをお願いします～
-${JSON.stringify(req.body)}
+${postingId}
     `;
     res.send({
       response_type: "in_channel",
@@ -68,32 +74,67 @@ ${JSON.stringify(req.body)}
 });
 
 export const reaction = functions.https.onRequest(async (req, res) => {
-  
-  await rp({
-    method: "POST",
-    uri:
-      "https://hooks.slack.com/services/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",
-    body: {
-      response_type: "in_channel", // "in_channel"
-      text: JSON.stringify(req.body),
-      // attachments: [
-      //   {
-      //     text: '',
-      //     callback_id: "invite",
-      //     actions: [
-      //       {
-      //         name: userName,
-      //         text: "仕事を依頼する",
-      //         type: "button",
-      //         style: "danger",
-      //         value: userId
-      //       }
-      //     ]
-      //   }
-      // ],
-    },
-    json: true // Automatically stringifies the body to JSON
-  });
-  
-  res.send("Hello from Firebase!");
+  const {
+    event
+    // token
+  } = req.body;
+
+  const {
+    // type,
+    channel,
+    ts
+  } = event.item;
+  // if (type !== "message") return;
+
+  const slackClient = new WebClient(accessToken);
+  try {
+    const postInfo: any = await slackClient.channels.history({
+      inclusive: true,
+      channel,
+      count: 1,
+      latest: ts
+    });
+    if (!postInfo || !postInfo.messages || postInfo.messages.length === 0) {
+      throw { status: 404, code: "Post not found" };
+    }
+    const message = postInfo.messages[0];
+    const _msgs = message.split('興味のある方はreactionをお願いします～\n');
+    if (_msgs.length <= 1) throw { msg: "invalid message" }
+    const postingId = _msgs[1]
+    const reactionsNum =  (message.reactions || []).reduce((count: number, el: any) => {
+        return count + el.count;
+      }, 0);
+    await rp({
+      method: "POST",
+      uri: " https://us-central1-inner-meetup.cloudfunctions.net/updatePosting",
+      body: {
+        postingId, // 投稿のID（tsと同じでよき）
+        reactionsNum, // reactionの数
+      },
+      json: true // Automatically stringifies the body to JSON
+    });
+    await rp({
+      method: "POST",
+      uri:
+        "https://hooks.slack.com/services/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",
+      body: {
+        response_type: "in_channel", // "in_channel"
+        text: JSON.stringify({ reactionsNum, postInfo })
+      },
+      json: true // Automatically stringifies the body to JSON
+    });
+  } catch (err) {
+    await rp({
+      method: "POST",
+      uri:
+        "https://hooks.slack.com/services/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",
+      body: {
+        response_type: "in_channel", // "in_channel"
+        text: JSON.stringify({ err })
+      },
+      json: true // Automatically stringifies the body to JSON
+    });
+  }
+
+  res.send({});
 });
