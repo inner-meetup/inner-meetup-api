f23e05636e4eba82afb1a3d08277cc95eb9f86cf
Aoi <tsuno-a@aidemy.net>
2019-09-14 17:28:58 +0900

    update いろいろ

--- functions/src/apis/cf_db.ts
+++ functions/src/apis/cf_db.ts
@@ -15,8 +15,7 @@ export const posting = functions.https.onRequest(
       description,
       postingType,
       userId,
-      channel,
-      ts
+      channel
     } = request.body;
 
     await db
@@ -26,7 +25,6 @@ export const posting = functions.https.onRequest(
         postingType,
         userId,
         channel,
-        ts,
         reactionsNum: 0
       })
       .then(res => {
@@ -52,7 +50,6 @@ export const updatePosting = functions.https.onRequest(
           channel: searchedPosting.channel,
           description: searchedPosting.description,
           postingType: searchedPosting.postingType,
-          ts: searchedPosting.ts,
           reactionsNum
         };
         db.ref().update(updates);
@@ -67,19 +64,41 @@ export const updatePosting = functions.https.onRequest(
 
 export const postRankingToSlack = functions.https.onRequest(
   async (request: any, response: any) => {
-    const text = `aaa`;
-    const payload = {
-      channel: "inner-meetup",
-      username: "人気のある勉強会はこちら！",
-      text,
-      icon_emoji: ":man-bowing::skin-tone-6:"
-    };
-    await rp({
-      uri:
-        "https://hooks.slack.com/services/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",
-      method: "POST",
-      form: `payload=${JSON.stringify(payload)}`,
-      json: true
-    });
+    await db
+      .ref("postings")
+      .once("value")
+      .then(async function(snapshot) {
+        const postings = snapshot.val() || {};
+        console.log("searchedPosting", Object.keys(postings));
+        const postingObjs = await Promise.all(
+          Object.keys(postings).map(async (_posting: string) => {
+            const postingObj = await db
+              .ref("postings/" + _posting)
+              .once("value");
+            return postingObj;
+          })
+        );
+        const sortedpostingObjs = postingObjs.sort(
+          (prevPosting: any, nextPosting: any) => {
+            return prevPosting.reactionsNum - nextPosting.reactionsNum;
+          }
+        );
+        console.log("postingObjs", postingObjs);
+        const text = `JSON.stringify(payload)`;
+        const payload = {
+          channel: "inner-meetup",
+          username: "人気のある勉強会はこちら！",
+          text,
+          icon_emoji: ":man-bowing::skin-tone-6:"
+        };
+        await rp({
+          uri:
+            "https://hooks.slack.com/services/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",
+          method: "POST",
+          form: `payload=${JSON.stringify(payload)}`,
+          json: true
+        });
+      });
+    response.send("Posting To Slack Successed");
   }
 );
