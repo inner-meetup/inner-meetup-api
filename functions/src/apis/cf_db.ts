e18e5ab15c69b50c7d8d84015335e57909cb4a67
Aoi <tsuno-a@aidemy.net>
2019-09-14 15:29:05 +0900

    add post

--- functions/src/apis/cf_db.ts
+++ functions/src/apis/cf_db.ts
@@ -11,13 +11,38 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const posting = functions.https.onRequest((request, response) => {
-  const db = firebase.database();
-  db.ref("users/" + "a").set({
-    username: "userId"
-  });
-  response.send("Hello from Firebase!");
-});
+export const posting = functions.https.onRequest(
+  async (request: any, response: any) => {
+    const {
+      postingId,
+      description,
+      postingType,
+      userId,
+      channel,
+      ts
+    } = request.body;
+
+    const db = firebase.database();
+    await db
+      .ref("postings/" + "posting")
+      .set({
+        postingId,
+        description,
+        postingType,
+        userId,
+        channel,
+        ts,
+        reactions: []
+      })
+      .then(res => {
+        console.log("res", res);
+      })
+      .catch(err => {
+        console.log("err", err);
+      });
+    response.send("Hello from Firebase!");
+  }
+);
 
 export const updatePosting = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
